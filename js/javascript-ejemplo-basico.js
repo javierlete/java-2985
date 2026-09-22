5da596a4315f204fe1ed0cf894eb384118f0dacb
Javier <javier@eskura21.com>
2026-09-22 17:39:41 +0200

    Ejemplo de JavaScript muy básico

---
+++ js/javascript-ejemplo-basico.js
@@ -0,0 +1,23 @@
+/*
+ * Nuestro primer ejemplo de JavaScript
+ * Haremos cosas pequeñas para empezar
+ */
+
+console.log('Hola mundo'); // muestra una texto en consola
+
+console.log('\'');
+console.log("'");
+console.log("\"")
+console.log('"');
+
+console.log('Hola que tal\nComo estamos');
+
+console.log(`
+Texto
+multilínea`);
+
+console.log(5 + 5);
+
+console.log(5 / 0);
+
+console.log(Math.sqrt(4));
