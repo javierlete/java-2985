f525e52270d49e0e98941330955449ea9dd561d1
Javier <javier@eskura21.com>
2026-09-23 17:50:33 +0200

    Sentencias de control

---
+++ js/sentencias-control.js
@@ -0,0 +1,13 @@
+let contador = 1;
+
+while (contador <= 10) {
+    if (contador % 2 === 0) {
+        console.log(contador);
+    }
+    
+    contador++;
+}
+
+for (let c = 1; c <= 10; c++) {
+    console.log(c);
+}
